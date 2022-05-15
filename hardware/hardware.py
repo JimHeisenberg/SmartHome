b5243b58543cd1df0c9425efe4d7b8e42fea5a02
JimHeisenberg <JimHeisenberg@hotmail.com>
2022-05-15 20:51:46 +0800

    update hardware

--- hardware/hardware.py
+++ hardware/hardware.py
@@ -13,20 +13,21 @@ BACKEND_URL = "http://"+BACKEND_IP+":12345"
 
 if __name__ == "__main__":
     cm = ConnectionManagerLocal(BACKEND_URL)
+    Addr = (BACKEND_IP, BACKEND_PORT)
     with cm as deviceManager:
-        s1 = deviceManager._createLocalSocket(BACKEND_IP, 54321)
-        light = Light(s1, None, deviceManager, 1, True)
-        light.send({"DeviceID": 1, "DeviceIsOn": True})
+        s1 = deviceManager._createLocalSocket(BACKEND_IP, BACKEND_PORT)
+        light = Light(s1, Addr, deviceManager, 1, True)
         light.tryChangeStatus(True)
+        light.send({"DeviceID": 1, "DeviceIsOn": True})
         deviceManager.addDeviceHandle(light)
 
         s3 = deviceManager._createLocalSocket(BACKEND_IP, 54321)
-        alarm = Alarm(s3, None, deviceManager, 3, True)
-        alarm.send({"DeviceID": 3, "DeviceIsOn": True})
+        alarm = Alarm(s3, Addr, deviceManager, 3, False)
+        alarm.send({"DeviceID": 3, "DeviceIsOn": False})
         deviceManager.addDeviceHandle(alarm)
 
         s5 = deviceManager._createLocalSocket(BACKEND_IP, 54321)
-        assistant = Assistant(s5, None, deviceManager, 5, True)
+        assistant = Assistant(s5, Addr, deviceManager, 5, True)
         assistant.send({"DeviceID": 5, "DeviceIsOn": True})
         deviceManager.addDeviceHandle(assistant)
     cm.start()
